a8cf9fd8d2841caa19a0b5e13d8ff79ebef3de1f
Stephen Chan <sychan@DOE6898194.local>
2012-04-04 12:22:02 -0700

    Some extra stub code.

--- libs/AuthClient.pm
+++ libs/AuthClient.pm
@@ -1,31 +1,45 @@
 package AuthClient;
 
+use Object::Tiny::RW qw { user logged_in error_msg };
+use AuthUser;
+use MIME::Base64;
+
 sub new() {
     $class = shift @_;
-    $self = {};
+    $self = { 'user' => AuthUser::new(),
+	      'logged_in' => 0,
+	      'error_msg' => ""};
     bless $self,$class;
-
     return $self;
 }
 
 sub login() {
     $self = shift @_;
+
+    return(1);
 }
 
 sub sign_request() {
     $self = shift @_;
+
+    return(1);
 }
 
 sub auth_token() {
     $self = shift @_;
+
+    return( encode_base64( "This is a token for " . $self->{'user'}->{'user_id'}));
 }
 
 sub new_consumer() {
     $self = shift @_;
+
+    return(1);
 }
 
 sub logout(){
     $self = shift @_;
+    return(1);
 }
 
 1;
