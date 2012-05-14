ece968b65c48a6a3fd67bb6635e27bcb6a701a0f
Ubuntu <ubuntu@sychan-test.novalocal>
2012-05-14 17:23:51 +0000

    updated the settings to point at the sqllite file

--- directory_server/AuthSvc/settings.py
+++ directory_server/AuthSvc/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/sychan/django/hello/.sqlite3/hello.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/kb/deployment/directory_server/AuthSvc/.sqlite/hello.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
