73bd2c702b2ccda323e9ce70ac431da0e1aac4a5
Bill Riehl <wjriehl@lbl.gov>
2013-08-06 12:14:45 -0400

    Updated exception handling - the login and verification methods should only throw AuthException (new class) or RuntimeException.

---
+++ java-libs/src/us/kbase/auth/AuthService.java
@@ -0,0 +1,389 @@
+package us.kbase.auth;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPublicKey;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMReader;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+/**
+ * A fairly simple Auth service client for KBase, intended (at least initially) for use with GLAMM.
+ * Usage:
+ * 
+ * AuthUser user = AuthService.login(user, password);
+ * if (AuthService.validateToken(user.getToken())) {
+ * 		// There's a valid token! Return the valid user, or just the token, and move along.
+ * }
+ * 
+ * Thus, this provides code for a user to log in to KBase, retrieve a valid Auth token, and
+ * optionally validate it.
+ * 
+ * @author wjriehl
+ */
+public class AuthService {
+	private static String AUTH_URL = "https://www.kbase.us/services/authorization";
+	private static String AUTH_LOGIN_PATH = "/Sessions/Login";
+	
+	/**
+	 * Logs in a user and returns an AuthUser object, which is more or less a POJO containing basic user attributes,
+	 * along with the generated AuthToken.
+	 * 
+	 * @param user
+	 * @param pw
+	 * @return an AuthUser that has been successfully logged in.
+	 * @throws AuthException if the credentials are invalid, or if there is a problem communicating with the server.
+	 */
+	public static AuthUser login(String userName, String password) throws AuthException {
+		// This is the data that will be POSTed to the service.
+		// By default (not sure if we *really* need to change it), it fetches all the fields.
+		try {
+			String dataStr = "user_id=" + URLEncoder.encode(userName, "UTF-8") + 
+							 "&password=" + URLEncoder.encode(password, "UTF-8") + 
+							 "&cookie=1&fields=user_id,name,email,groups,kbase_sessionid,token,verified,opt_in,system_admin";
+			return fetchUser(dataStr);
+		}
+		catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("An unexpected URL encoding exception occurred: " + e.getLocalizedMessage());
+		}
+	}
+
+	/**
+	 * Given a token for a logged in user, returns the AuthUser object representing that user's profile.
+	 *
+	 * @param token
+	 * @return an AuthUser associated with the given token.
+	 * @throws AuthException if the token is malformed or invalid, or if an error occurs while communicating
+	 * with the server.
+	 */
+	public static AuthUser getUserFromToken(String token) throws AuthException {
+		String dataStr = "token=" + token +
+						 "&fields=user_id,name,email,groups,kbase_sessionid,token,verified,opt_in,system_admin";
+		
+		return fetchUser(dataStr);
+	}
+
+	/**
+	 * Given an AuthToken object for a logged in user, this returns the AuthUser object representing that user's
+	 * profile.
+	 * 
+	 * @param token
+	 * @return an AuthUser associated with the given token.
+	 * @throws AuthException 
+	 */
+	public static AuthUser getUserFromToken(AuthToken token) throws AuthException {
+		
+		String dataStr = "token=" + token.toString() +
+						 "&fields=user_id,name,email,groups,kbase_sessionid,token,verified,opt_in,system_admin";
+		
+		return fetchUser(dataStr);
+	}
+	
+	/**
+	 * Given a data str, describing URL-encoded fields for the auth server, this attempts to authenticate the user
+	 * with KBase servers.
+	 * 
+	 * @param dataStr 
+	 * @return an AuthUser that has been authenticated with KBase
+	 * @throws AuthException if the auth credentials are invalid or if the login URL is incorrect.
+	 */
+	private static AuthUser fetchUser(String dataStr) throws AuthException {
+		// Start with a null user - if the mapper fails for some reason, we know it's
+		// still null (and not uninitialized), and can throw a proper exception.
+		AuthUser user = null;
+
+		try {
+			// Build the connection project and set it up.
+			HttpsURLConnection conn = (HttpsURLConnection) new URL(AUTH_URL + AUTH_LOGIN_PATH).openConnection();
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			conn.setRequestProperty("Content-Length", String.valueOf(dataStr.getBytes().length));
+			conn.setRequestProperty("Content-Language", "en-US");
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(false);
+			
+			// Write out the POST data.
+			DataOutputStream writer = new DataOutputStream(conn.getOutputStream());
+			writer.writeBytes(dataStr);
+			writer.flush();
+			writer.close();
+			
+			// If we don't have a happy response code, throw an exception.
+			int responseCode = conn.getResponseCode();
+			if (responseCode != 200) {
+				conn.disconnect();
+				throw new AuthException("Login failed! Server responded with code " + responseCode + " " + conn.getResponseMessage());
+			}
+
+			/** Encoding the HTTP response into JSON format */
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	
+			user = new ObjectMapper().readValue(br, AuthUser.class);
+
+			br.close();
+			conn.disconnect();
+		}
+		catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("An unexpected encoding exception occurred: " + e.getLocalizedMessage());
+		}
+		catch (MalformedURLException e) {
+			throw new RuntimeException("An unexpected URL exception occurred: " + e.getLocalizedMessage());
+		}
+		catch (IOException e) {
+			throw new AuthException("An exception occurred while parsing authentication results: " + e.getLocalizedMessage());
+		}
+		
+		// In the end, return the user.
+		if (user == null) { // if still null, throw an exception 
+			throw new AuthException("Unable to construct a user object from login results!");
+		}
+		return user;
+		
+	}
+	
+	/**
+	 * Given a String representation of an auth token, this validates it against its source in Globus Online.
+	 * 
+	 * @param tokenStr the token string retrieved from KBase
+	 * @return true if the token's valid, false otherwise
+	 * @throw AuthException if there is a problem parsing the token or the server response, or if the token's 
+	 * verification URL is invalid.
+	 */
+	public static boolean validateToken(String tokenStr) throws AuthException {
+		try {
+			AuthToken token = new AuthToken(tokenStr);
+			return validateToken(token);
+		}
+		catch (IOException e) {
+			throw new AuthException("An error occurred while parsing your token: " + e.getLocalizedMessage());
+		}
+	}
+	
+	/**
+	 * This validates a KBase Auth token, and returns true or if valid or false if not.
+	 * 
+	 * It works in a few steps:
+	 * 1. Check the token format, throw IOException if the format's wrong.
+	 * 2. Extract the 'token' part and the signature.
+	 * 3. Get the public key from the 'SigningSubject' part of the token.
+	 * 4. Do the sha1-RSA comparison.
+	 * 5. Return the result of the comparison
+	 * 
+	 * Most of this is taken from KBaseAuthValidateToken.java, written by Shuchu Han, as part of the Persistent Store code.
+	 * So, thanks!
+	 *
+	 * @param token
+	 * @return true if the token's valid, false otherwise
+	 * @throws AuthException if there is a parsing problem with either the token or the server response, or if the token's 
+	 * verification URL is invalid.
+	 */
+	public static boolean validateToken(AuthToken token) throws AuthException {		
+		boolean result = false;
+
+		try {
+			/** now HTTPS the SigningSubject of input Token */
+			URL validationUrl = new URL(token.getSigningSubject());
+			
+			// Create a trust manager that does not validate certificate chains
+			TrustManager[] trustAllCerts = new TrustManager[] { 
+				new X509TrustManager() {
+					public X509Certificate[] getAcceptedIssuers() { 
+						return new X509Certificate[0]; 
+					}
+					public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+					public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+				}
+			};
+			
+			// Ignore differences between given hostname and certificate hostname
+			HostnameVerifier hv = new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) { return true; }
+			};
+			
+			// Install the all-trusting trust manager
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultHostnameVerifier(hv);
+			
+			/** make the request to Authentication server */
+			HttpsURLConnection conn = (HttpsURLConnection) validationUrl.openConnection(); 
+			InputStream in = conn.getInputStream();
+			
+			/** Encoding the HTTP response into JSON format */
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			ObjectMapper m = new ObjectMapper();
+			JsonNode jn = m.readTree(br);
+			JsonNode jd = jn.get("pubkey");
+
+			in.close();
+			conn.disconnect();
+			
+			/** now get the public key and do the verify */
+			Security.addProvider(new BouncyCastleProvider());
+			PEMReader pemReader = new PEMReader(new StringReader(jd.getTextValue().replace("\\n","\n")));
+			RSAPublicKey pubKey = (RSAPublicKey) pemReader.readObject();
+			pemReader.close();
+
+			/** http://docs.oracle.com/javase/6/docs/technotes/guides/security/crypto/CryptoSpec.html#KeyFactoryEx */
+			Signature s = Signature.getInstance("SHA1withRSA");
+			
+			s.initVerify(pubKey);
+
+			/** update the data
+			 * 
+			 *	SHA-1 (and all other hashing algorithms) return binary data. That means that (in Java) they produce a byte[].
+			 *  That byte array does not represent any specific characters, which means you can't simply turn it into a String 
+			 *  like you did.If you need a String, then you have to format that byte[] in a way that can be represented as 
+			 *  a String (otherwise, just keep the byte[] around).Two common ways of representing arbitrary byte[] as printable
+			 *  characters are BASE64 or simple hex-Strings (i.e. representing each byte by two hexadecimal digits). 
+			 *  It looks like you're trying to produce a hex-String. There's also another pitfall: if you want to get the SHA-1 
+			 *  of a Java String, then you need to convert that String to a byte[] first (as the input of SHA-1 is a byte[] as well). 
+			 *  If you simply use myString.getBytes() as you showed, then it will use the platform default encoding and as such 
+			 *  will be dependent on the environment you run it in (for example it could return different data based on the 
+			 *  language/locale setting of your OS).A better solution is to specify the encoding to use for the String-to-byte[] 
+			 *  conversion like this: myString.getBytes("UTF-8"). Choosing UTF-8 (or another encoding that can represent every 
+			 *  unicode character) is the safest choice here.
+			 */
+			byte[] sig_data_byte = token.getTokenData().getBytes("UTF-8");
+			s.update(sig_data_byte);
+
+			/**
+			* The equivalent of Perl 's pack "H*", $vartoconvert in Java is :
+			* javax.xml.bind.DatatypeConverter.parseHexBinary(hexadecimalString);.
+			* For more information on this, I think it is recommended to read 
+			* DatatypeConverter class' reference from JavaDocs. 
+			*/
+			byte[] sig_byte = javax.xml.bind.DatatypeConverter.parseHexBinary(token.getSignature());
+			
+			/** verification of signature*/
+			result = s.verify(sig_byte);
+		}
+		catch (SignatureException e) {
+			throw new RuntimeException("An unexpected error occurred while verifying the auth token");
+		}
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("An unexpected error occurred while verifying the auth token: " + e.getLocalizedMessage());
+		}
+		catch (MalformedURLException e) {
+			throw new AuthException("An error occurred while using the token's verification URL: " + e.getLocalizedMessage());
+		}
+		catch (IOException e) {
+			throw new AuthException("An error occurred while retrieving the token signature");
+		}
+		catch (KeyManagementException e) {
+			throw new RuntimeException("An unexpected error occurred while initializing the SSL trust mechanism.");
+		}
+		catch (InvalidKeyException e) {
+			throw new AuthException("An error occurred while parsing the public key for the token.");
+		}
+		return result;
+	}
+
+	/**
+	 * Disables SSL certificate validation.
+	 * 
+	 * Once upon a time, we had issues getting the KBase SSL certificate renewed. This isn't a big deal for a web
+	 * user - just click through the "Hey, this isn't valid!" page. But going over https would make the Java
+	 * HttpsURLConnection have a tizzy fit.
+	 * 
+	 * Running this method tells the HttpsURLConnection to ignore any certificate validation errors in any subsequent
+	 * calls. So if the certificate has errors in the future, run this method before doing any service calls.
+	 * 
+	 * e.g.
+	 * <code>
+	 * AuthService.disableCertificateValidation();
+	 * AuthService.login(<<credentials>>);
+	 * </code>
+	 */
+	@SuppressWarnings("unused")
+	private static void disableCertificateValidation() {
+		TrustManager[] trustAllCerts = new TrustManager[] {
+			new X509TrustManager() {
+				public X509Certificate[] getAcceptedIssuers() {
+					return new X509Certificate[0];
+				}
+				public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+				public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+			}
+		};
+		
+		HostnameVerifier hv = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) { return true; }
+		};
+		
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultHostnameVerifier(hv);
+		} catch (Exception e) {}
+	}
+	
+	/**
+	 * Returns the current URL to which the service is pointing.
+	 * @return the current URL for the service
+	 */
+	public static String getServiceUrl() {
+		return AUTH_URL;
+	}
+	
+	/**
+	 * Sets the URL that the service should point to. This is the base URL, e.g.:
+	 * https://kbase.us/services/authorization
+	 * NOT
+	 * https://kbase.us/services/authorization/Sessions/Login
+	 * 
+	 * @param url
+	 */
+	public static void setServiceUrl(String url) {
+		if (url != null && url.length() > 0 && url.startsWith("http"))
+			AUTH_URL = url;
+	}
+	
+	// Main for testing.
+	// TODO - make some JUnit tests.
+	public static void main(String[] args) {
+		try {
+			AuthUser user = AuthService.login("kbasetest", "@Suite525");
+			boolean validated = AuthService.validateToken(user.getToken());
+			System.out.println(user.toString() + "\nValidated token? " + validated);
+			
+			AuthUser user2 = AuthService.getUserFromToken(user.getToken().toString());
+			System.out.println(user2.toString());
+			System.out.println(user2.getToken().getTokenData());
+			
+			System.out.println(user2.getToken().isExpired());
+		} catch (AuthException e) {
+			// handle IOException
+			System.out.println(e.getLocalizedMessage());
+		}
+	}
+	
+}
