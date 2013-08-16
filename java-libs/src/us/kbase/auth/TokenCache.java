b8d998234232a374a37339d041fbfc4fd0275a78
Gavin Price <gaprice@lbl.gov>
2013-08-16 10:31:07 -0700

    Thread safe token cache

--- java-libs/src/us/kbase/auth/TokenCache.java
+++ java-libs/src/us/kbase/auth/TokenCache.java
@@ -5,31 +5,74 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Caches tokens to avoid network queries to the token provider.
+ * 
+ * Tokens are stored until the size of the cache is greater than the maximum
+ * allowed size. Tokens are then ordered by most recent access and the oldest
+ * tokens are discared to return the cache to its nominal size.
+ * 
+ * This class is thread safe.
+ * 
+ * @author gaprice@lbl.gov
+ *
+ */
 public class TokenCache {
 	
-	final static int DEFAULT_SIZE = 1000;
-	final static int DEFAULT_MAX_SIZE = 1000;
+	/**
+	 * Default nominal size of the cache.
+	 */
+	final public static int DEFAULT_SIZE = 1000;
+	/**
+	 * Default maximum size of the cache.
+	 */
+	final public static int DEFAULT_MAX_SIZE = 2000;
 	
 	final private int size;
 	final private int maxsize;
-	final private HashMap<String, Date> cache = new HashMap<String, Date>();
+	final private ConcurrentHashMap<String, Date> cache = new ConcurrentHashMap<String, Date>();
 	
+	/**
+	 * Create a new TokenCache.
+	 * @param size the nominal size of the cache in tokens
+	 * @param maxsize the maximum size of the cache in tokens
+	 */
 	public TokenCache(int size, int maxsize) {
+		if (size < 1 || maxsize < 1) {
+			throw new IllegalArgumentException("size and maxsize must be > 0");
+		}
+		if (size >= maxsize) {
+			throw new IllegalArgumentException("size must be < maxsize");
+		}
 		this.size = size;
 		this.maxsize = maxsize;
 	}
 	
+	/**
+	 * Create a new TokenCache with the default max size.
+	 * @param size the nominal size of the cache in tokens
+	 */
 	public TokenCache(int size) {
 		this(size, DEFAULT_MAX_SIZE);
 	}
 	
+	/**
+	 * Create a new TokenCache with default parameters.
+	 */
 	public TokenCache() {
 		this(DEFAULT_SIZE);
 	}
 	
+	/**
+	 * Determine whether a token is in the cache.
+	 * @param token the token to check
+	 * @return <code>true</code> if the token is in the cache, <code>false</code>
+	 * otherwise.
+	 * @throws TokenExpiredException if the token is expired.
+	 */
 	public boolean hasToken(AuthToken token) throws TokenExpiredException {
 		return checkToken(token).tokenInCache;
 	}
@@ -46,18 +89,25 @@ public class TokenCache {
 		return new MD5Bool(false, tokmd);
 	}
 	
+	/**
+	 * Add a token to the cache. This method assumes the token is valid.
+	 * @param token the token to add
+	 * @throws TokenExpiredException if the token is expired.
+	 */
 	public void putValidToken(AuthToken token) throws TokenExpiredException {
 		final MD5Bool sb = checkToken(token);
 		if(sb.tokenInCache) {return;}
 		cache.put(sb.tokenMD5, new Date());
-		if(cache.size() <= maxsize) {return;}
-		List<DateMD5> dmd5s = new ArrayList<DateMD5>();
-		for (String s: cache.keySet()) {
-			dmd5s.add(new DateMD5(cache.get(s), s));
-		}
-		Collections.sort(dmd5s);
-		for(int i = size; i < dmd5s.size(); i++) {
-			cache.remove(dmd5s.get(i).md5);
+		synchronized (cache) { // block here otherwise all threads may start sorting
+			if(cache.size() <= maxsize) {return;}
+			List<DateMD5> dmd5s = new ArrayList<DateMD5>();
+			for (String s: cache.keySet()) {
+				dmd5s.add(new DateMD5(cache.get(s), s));
+			}
+			Collections.sort(dmd5s);
+			for(int i = size; i < dmd5s.size(); i++) {
+				cache.remove(dmd5s.get(i).md5);
+			}
 		}
 	}
 	
@@ -72,16 +122,16 @@ public class TokenCache {
 	}
 
 	// from http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
-	final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	final private static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	private static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    int v;
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
+		char[] hexChars = new char[bytes.length * 2];
+		int v;
+		for ( int j = 0; j < bytes.length; j++ ) {
+			v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
 	}
 }
 
