7b1fbe13e360486756d011eb1dbba267d94a9844
Bill Riehl <wjriehl@lbl.gov>
2013-08-15 17:47:48 -0700

    More java updates: removed bouncycastle libs from the makefile, updated AuthService.java to use URL objects instead of strings, and throw IOExceptions when there's a network issue.

--- java-libs/test/AuthServiceTest.java
+++ java-libs/test/AuthServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.matchers.JUnitMatchers.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
+import java.net.URL;
 
 import us.kbase.auth.AuthService;
 import us.kbase.auth.AuthToken;
@@ -230,16 +231,18 @@ public class AuthServiceTest {
 	}
 
 	@Test
-	public void testServiceUrlChange() {
-		String oldUrl = AuthService.getServiceUrl();
-		String newUrl = "https://kbase.us/testurl";
-		AuthService.setServiceUrl(newUrl);
-		org.junit.Assert.assertEquals("failure - new url isn't set properly", newUrl, AuthService.getServiceUrl());
+	public void testValidServiceUrlChange() throws Exception{
+		URL oldUrl = AuthService.getServiceUrl();
+
+		// valid url
+		URL newUrl = new URL("https://kbase.us/services/authorization/Sessions/Login");
+		
+		org.junit.Assert.assertTrue("failure - setting a valid service url didn't work!", AuthService.setServiceUrl(newUrl));
 		AuthService.setServiceUrl(oldUrl);
 	}
 
 	@Test
-	public void testGetUserFromTokenObject() throws AuthException {
+	public void testGetUserFromTokenObject() throws Exception {
 		AuthToken t = new AuthToken(testUser.getToken().toString(), 400);
 		AuthUser user = AuthService.getUserFromToken(t);
 		org.junit.Assert.assertNotNull("failure - getting user from a token object returned a null user", user);
@@ -247,13 +250,13 @@ public class AuthServiceTest {
 	}
 
 	@Test
-	public void testLogin() throws AuthException {
+	public void testLogin() throws Exception {
 		AuthUser user = AuthService.login(TEST_UID, TEST_PW);
 		org.junit.Assert.assertNotNull("failure - logging in returned a null user", user);
 	}
 	
 	@Test
-	public void testLoginWithExpiry() throws AuthException {
+	public void testLoginWithExpiry() throws Exception {
 		AuthUser user = AuthService.login(TEST_UID, TEST_PW, 300);
 		assertEquals("fail - wrong expiration lifetime", 300, user.getToken().getExpiryTime());
 	}
@@ -272,7 +275,7 @@ public class AuthServiceTest {
 
 	// login with bad user/pw
 	@Test(expected = AuthException.class)
-	public void testFailLogin() throws AuthException {
+	public void testFailLogin() throws Exception {
 		AuthService.login("asdf", "asdf");
 	}
 
